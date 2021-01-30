e7eb43291247dbf8bbaaa3a65b614cb71a199e08
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-30 18:41:20 +0300

    imported button component to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "./components/Button/index";
 
 function App() {
-  return <div className="wrapper">Hello world</div>;
+  return (
+    <div className="wrapper">
+      <h1>Hello</h1>
+      <Button>Click</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button as BaseButton } from "antd";
+import "./Button.scss";
 
 export default function Button({ children }) {
   return <BaseButton>{children}</BaseButton>;
