595de63819eba39a9840adaea96e4c7c8368a8a7
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-30 18:56:54 +0300

    added prop types for btn

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import propTypes from "prop-types";
 import { Button as BaseButton } from "antd";
 import "./Button.scss";
 
-export default function Button({ children }) {
-  return <BaseButton>{children}</BaseButton>;
+export default function Button(props) {
+  return <BaseButton className="button" {...props}></BaseButton>;
 }
+
+Button.propTypes = {
+  className: propTypes.string,
+};
