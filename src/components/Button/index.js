346f06370d13cda22675a942fd1126893576d1d1
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-30 19:17:56 +0300

    import classname to btn component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import propTypes from "prop-types";
+import classNames from "classnames";
 import { Button as BaseButton } from "antd";
 import "./Button.scss";
 
 export default function Button(props) {
-  return <BaseButton className="button" {...props}></BaseButton>;
+  return (
+    <BaseButton
+      className={classNames("button", props.className)}
+      {...props}
+    ></BaseButton>
+  );
 }
 
 Button.propTypes = {
