2fe9c93662bd377b863f0cee6f40536eaf3e0b99
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-31 19:39:10 +0300

    created pages structure; replaced auth page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React from "react";
 import { Button, Block } from "components";
 
 function App() {
-  return (
-    <div className="wrapper">
-      <Block>
-        <Button type="primary" size="large">
-          Click
-        </Button>
-      </Block>
-    </div>
-  );
+  return <div className="wrapper"></div>;
 }
 
 export default App;
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div>
+      <Block>
+        <Button type="primary" size="large">
+          Click
+        </Button>
+      </Block>
+    </div>
+  );
+};
+
+export default Auth;
