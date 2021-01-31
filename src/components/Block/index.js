6f6664e5abce509670d38ecc451329dd9c54035e
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-31 18:49:06 +0300

    added block; deleted white block

---
+++ src/components/Block/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import propTypes from "prop-types";
+import "./Block.scss";
+
+const Block = ({ children }) => <div className="block">{children}</div>;
+
+export default Block;
