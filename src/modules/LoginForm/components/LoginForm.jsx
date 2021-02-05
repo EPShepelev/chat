244d5658f535da8b92ea722cdb9799d07cd1e1ce
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-05 15:33:18 +0300

    rewrite login form component

--- src/modules/LoginForm/components/LoginForm.jsx
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -1,47 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import { Form, Input } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { Button, Block } from "components";
 
-export default class LoginForm extends Component {
-  render() {
-    return (
-      <div>
-        <div className="auth__top">
-          <LoginForm />
-          <h2>Войти в аккаунт</h2>
-          <p>Пожалуйста, войдите в свой аккаунт</p>
-        </div>
-        <Block>
-          <Form className="login-form" name="basic">
-            <Form.Item>
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-                size="large"
-              />
-            </Form.Item>
-
-            <Form.Item>
-              <Input.Password
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                placeholder="Password"
-                size="large"
-              />
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" size="large">
-                Войти в аккаунт
-              </Button>
-            </Form.Item>
-            <Link className="auth__register-link" to="/registration">
-              Зарегистрироваться
-            </Link>
-          </Form>
-        </Block>
+const LoginForm = () => {
+  return (
+    <div>
+      <div className="auth__top">
+        <LoginForm />
+        <h2>Войти в аккаунт</h2>
+        <p>Пожалуйста, войдите в свой аккаунт</p>
       </div>
-    );
-  }
-}
+      <Block>
+        <Form className="login-form" name="basic">
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Password"
+              size="large"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" size="large">
+              Войти в аккаунт
+            </Button>
+          </Form.Item>
+          <Link className="auth__register-link" to="/registration">
+            Зарегистрироваться
+          </Link>
+        </Form>
+      </Block>
+    </div>
+  );
+};
+
+export default LoginForm;
