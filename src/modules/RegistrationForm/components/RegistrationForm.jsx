a2e467a4abe4c9a5d9b5d77aac2a08207fcebb92
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-10 20:20:57 +0300

    added confirm block

--- src/modules/RegistrationForm/components/RegistrationForm.jsx
+++ src/modules/RegistrationForm/components/RegistrationForm.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Form, Input } from "antd";
-import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  LockOutlined,
+  MailOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { Button, Block } from "components";
 
 const RegistrationForm = () => {
+  const sucsess = true;
   return (
     <div>
       <div className="auth__top">
@@ -12,48 +18,61 @@ const RegistrationForm = () => {
         <p>Для входа в чат, вам нужно зарегистрироваться</p>
       </div>
       <Block>
-        <Form className="login-form" name="basic">
-          <Form.Item>
-            <Input
-              prefix={<MailOutlined className="site-form-item-icon" />}
-              placeholder="E-mail"
-              size="large"
-            />
-          </Form.Item>
+        {!sucsess ? (
+          <Form className="login-form" name="basic">
+            <Form.Item>
+              <Input
+                prefix={<MailOutlined className="site-form-item-icon" />}
+                placeholder="E-mail"
+                size="large"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Input.Password
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Ваше имя"
-              size="large"
-            />
-          </Form.Item>
+            <Form.Item>
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Ваше имя"
+                size="large"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Input.Password
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              placeholder="Пароль"
-              size="large"
-            />
-          </Form.Item>
+            <Form.Item>
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Пароль"
+                size="large"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Input.Password
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              placeholder="Повторите пароль"
-              size="large"
-            />
-          </Form.Item>
+            <Form.Item>
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Повторите пароль"
+                size="large"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" size="large">
-              Зарегистрироваться
-            </Button>
-          </Form.Item>
-          <Link className="auth__register-link" to="/login">
-            Войти в аккаунт
-          </Link>
-        </Form>
+            <Form.Item>
+              <Button type="primary" size="large">
+                Зарегистрироваться
+              </Button>
+            </Form.Item>
+            <Link className="auth__register-link" to="/login">
+              Войти в аккаунт
+            </Link>
+          </Form>
+        ) : (
+          <div className="auth__sucsess-block">
+            <div>
+              <ExclamationCircleOutlined style={{ fontSize: "50px" }} />
+            </div>
+            <h3>Подтвердите свой аккаунт</h3>
+            <p>
+              На вашу почту отправлено письмо со ссылкой на подтверждение
+              аккаунта.
+            </p>
+          </div>
+        )}
       </Block>
     </div>
   );
