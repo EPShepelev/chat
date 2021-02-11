69a79f973f3509acc70bb365f146c59ff4a4a492
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-11 14:36:17 +0300

    created basic home component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "./Home.scss";
+
+const Home = () => (
+  <section className="home">
+    <h1>Home component</h1>
+  </section>
+);
+
+export default Home;
