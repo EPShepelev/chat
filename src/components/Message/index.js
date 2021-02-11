78894d4f3d19afb6a6df093c6cf82564aed7daa8
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-11 14:48:12 +0300

    added message component

---
+++ src/components/Message/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Message as BaseButton } from "antd";
+import "./Message.scss";
+
+const Message = (props) => (
+  
+);
+
+Message.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Message;
