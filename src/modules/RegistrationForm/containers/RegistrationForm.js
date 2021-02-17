c6eea94abc9fd33983ce1ab55ad7e786de843e6d
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-17 15:37:49 +0300

    added with formik func

--- src/modules/RegistrationForm/containers/RegistrationForm.js
+++ src/modules/RegistrationForm/containers/RegistrationForm.js
@@ -1,3 +1,24 @@
 import RegistrationForm from "../components/RegistrationForm";
+import { withFormik } from "formik";
 
-export default RegistrationForm;
+export default withFormik({
+  mapPropsToValues: () => ({ name: "" }),
+  validate: (values) => {
+    const errors = {};
+
+    if (!values.name) {
+      errors.name = "Required";
+    }
+
+    return errors;
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: "RegistrationForm",
+})(RegistrationForm);
