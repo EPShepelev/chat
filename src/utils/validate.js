227dccfa32fe10a933d2c2cb4b01cfe4159216f9
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-19 21:57:47 +0300

    fixed validate.js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,13 +1,14 @@
 export default ({ isAuth, values, errors }) => {
 
   const rules = {
+    email: (value) =>{
     if (!value) {
       errors.email = "Введите email";
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
       errors.email = "Некорректный email";
     }
   },
-  password: (errors, value) => {
+  password: (value) => {
     if (!value) {
       errors.password = "Введите пароль!";
     } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(value)) {
@@ -18,7 +19,7 @@ export default ({ isAuth, values, errors }) => {
   }
 
   Object.keys(values).forEach(
-    (key) => rules[key] && rules[key](errors, values[key])
+    (key) => rules[key] && rules[key](values[key])
   );
 
   
