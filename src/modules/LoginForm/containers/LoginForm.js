be65bcffdf7c42215e7deda07b32c132f7050434
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-19 15:51:43 +0300

    created login form validation

--- src/modules/LoginForm/containers/LoginForm.js
+++ src/modules/LoginForm/containers/LoginForm.js
@@ -1,3 +1,25 @@
 import LoginForm from "../components/LoginForm";
+import { withFormik } from "formik";
+import validateForm from "utils/validate";
+
+export default withFormik({
+  validate: (values) => {
+    let errors = {};
+    const validate = validateForm({ isAuth: true });
+    Object.keys(values).forEach(
+      (key) => validate[key] && validate[key](errors, values[key])
+    );
+    return errors;
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: "LoginForm",
+})(LoginForm);
 
 export default LoginForm;
