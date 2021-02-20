52407f022d4eb5540127e670c18574e00bb086da
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-20 21:15:48 +0300

    deleted conditions

--- src/modules/LoginForm/components/LoginForm.jsx
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -21,68 +21,55 @@ const LoginForm = (props) => {
         <h2>Войти в аккаунт</h2>
         <p>Пожалуйста, войдите в свой аккаунт</p>
       </div>
-      <Block><Form className="login-form" onSubmit={handleSubmit}>
-            <Form.Item
-              validateStatus={
-                !touched.email ? "" : errors.email ? "error" : "success"
-              }
-              hasFeedback
-              help={!touched.email ? "" : errors.email}
-            >
-              <Input
-                id="email"
-                prefix={<MailOutlined className="site-form-item-icon" />}
-                placeholder="E-mail"
-                size="large"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Item>
+      <Block>
+        <Form className="login-form" onSubmit={handleSubmit}>
+          <Form.Item
+            validateStatus={
+              !touched.email ? "" : errors.email ? "error" : "success"
+            }
+            hasFeedback
+            help={!touched.email ? "" : errors.email}
+          >
+            <Input
+              id="email"
+              prefix={<MailOutlined className="site-form-item-icon" />}
+              placeholder="E-mail"
+              size="large"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Item>
 
-            <Form.Item
-              validateStatus={
-                !touched.password ? "" : errors.password ? "error" : "success"
-              }
-              hasFeedback
-              help={!touched.password ? "" : errors.password}
-            >
-              <Input.Password
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                placeholder="Пароль"
-                size="large"
-                id="password"
-                type="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Item>
+          <Form.Item
+            validateStatus={
+              !touched.password ? "" : errors.password ? "error" : "success"
+            }
+            hasFeedback
+            help={!touched.password ? "" : errors.password}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Пароль"
+              size="large"
+              id="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Item>
 
-            <Form.Item>
-              {isSubmitting && !isValid && <span>Ошибка!</span>}
-              <Button onClick={handleSubmit} type="primary" size="large">
-                Войти в аккаунт
-              </Button>
-            </Form.Item>
-            <Link className="auth__register-link" to="/login">
-              Зарегистрироваться
-            </Link>
-          </Form>
-        {!sucsess ? (
-          
-        ) : (
-          <div className="auth__sucsess-block">
-            <div>
-              <ExclamationCircleOutlined style={{ fontSize: "50px" }} />
-            </div>
-            <h2>Подтвердите свой аккаунт</h2>
-            <p>
-              На вашу почту отправлено письмо со ссылкой на подтверждение
-              аккаунта.
-            </p>
-          </div>
-        )}
+          <Form.Item>
+            {isSubmitting && !isValid && <span>Ошибка!</span>}
+            <Button onClick={handleSubmit} type="primary" size="large">
+              Войти в аккаунт
+            </Button>
+          </Form.Item>
+          <Link className="auth__register-link" to="/login">
+            Зарегистрироваться
+          </Link>
+        </Form>
       </Block>
     </div>
   );
