46f38f53af4be1ac7d162bdb126a5bb980f06e00
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-25 12:55:59 +0300

    added example avatar manualy

--- src/components/DialogItem/index.js
+++ src/components/DialogItem/index.js
@@ -5,7 +5,12 @@ import "./DialogItem.scss";
 const DialogItem = ({ user, message }) => (
   <div className="dialogs__item">
     <div className="dialogs__item-avatar">
-      <img src={user.avatar} alt={`${user.fullname} avatar`} />
+      {/* <img src={user.avatar} alt={`${user.fullname} avatar`} /> */}
+      <img src="https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-90.jpg></img>
+    </div>
+    <div className="dialogs__item-info">
+     <div className="dialogs__item-info-top"></div>
+
     </div>
   </div>
 );
