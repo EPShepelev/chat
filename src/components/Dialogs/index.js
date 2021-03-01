a95a19b16358ae1d0717ca57676e08a95828a759
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-01 20:34:42 +0300

    created dialogs component

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import classNames from "classnames";
 import "./Dialogs.scss";
 
-const Dialogs = ({ user, message, isMe, isRead }) => <div></div>;
+const Dialogs = ({ user, message, isMe, isRead }) => 
+  <div className="dialogs">
+      <DialogItem user={{
+        fullname: "усатый прибалт"
+        isOnline: false
+      }} isRead={999}/>
+      <DialogItem user={{
+        fullname: "усатый прибалт"
+        isOnline: true
+      }} isRead={0}/>
+      <DialogItem user={{
+        fullname: "усатый прибалт"
+        isOnline: false
+      }} isRead={5}/>
+    </div>;
+
 
 export default Dialogs;
