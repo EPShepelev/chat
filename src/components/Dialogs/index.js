399f6d7cd5c169a6d50b0b3eeb7f1c9f1ce0571a
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-02 17:51:22 +0300

    modifed dialog item

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -3,6 +3,12 @@ import classNames from "classnames";
 import "./Dialogs.scss";
 import { DialogItem } from "../";
 
-const Dialogs = ({ items }) => <div className="dialogs">{items}</div>;
+const Dialogs = ({ items }) => (
+  <div className="dialogs">
+    {items.map((item) => (
+      <DialogItem user={item.user} unread={999} />
+    ))}
+  </div>
+);
 
 export default Dialogs;
