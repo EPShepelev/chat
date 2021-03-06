d2b3377ec5658e06fd1ecfa5c17ec71d5d582ec7
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-06 19:13:25 +0300

    uncomment and created massage with audio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -32,26 +32,13 @@ const Home = () => (
         },
       ]}
     />
-    {/* {/* <Message */}
-    avatar="https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-90.jpg"
-    text="Ну что у нас там? Все хорошо?" date="Thu Feb 13 2021 17:12:14" isMe=
-    {false}
-    attachments=
-    {[
-      {
-        filename: "image.jpg",
-        url: "https://source.unsplash.com/100x100/?random=1&nature,water",
-      },
-      {
-        filename: "image.jpg",
-        url: "https://source.unsplash.com/100x100/?random=2&nature,water",
-      },
-      {
-        filename: "image.jpg",
-        url: "https://source.unsplash.com/100x100/?random=3&nature,water",
-      },
-    ]}
+    <Message
+      avatar="https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-90.jpg"
+      text="Ну что у нас там? Все хорошо?"
+      date="Thu Feb 13 2021 17:12:14"
+      audio="https://filesamples.com/samples/audio/wav/sample1.wav"
     />
+    {/*
     <Message
       avatar="https://pbs.twimg.com/profile_images/456803584504393728/Im9iEap0.jpeg"
       text="Да, все хорошо. Все сделали, нарушения будут"
