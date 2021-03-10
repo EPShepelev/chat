37c2c13ba1cebff62b0813e732429e860a97786e
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-10 15:31:34 +0300

    deleted copy message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -138,59 +138,6 @@ const Message = ({
   );
 };
 
-const Message1 = ({
-  avatar,
-  user,
-  text,
-  date,
-  isMe,
-  isRead,
-  attachments,
-  isTyping,
-}) => (
-  <div
-    className={classNames("message", {
-      "message--isme": isMe,
-      "message--istyping": isTyping,
-      "message--image": attachments && attachments.length === 1,
-    })}
-  >
-    <div className="message__content">
-      <IconRead isMe={isMe} isRead={isRead} />
-      <div className="message__avatar">
-        <img src={avatar} alt={`Avatar ${user.fullname}`}></img>
-      </div>
-      <div className="message__info">
-        {(text || isTyping) && (
-          <div className="message__bubble">
-            {text && <p className="message__text">{text}</p>}
-            {isTyping && (
-              <div className="message__typing">
-                <span className="dot one"></span>
-                <span className="dot two"></span>
-                <span className="dot three"></span>
-              </div>
-            )}
-          </div>
-        )}
-        <div className="message__attachments">
-          {attachments &&
-            attachments.map((item) => (
-              <div className="message__attachments-item">
-                <img src={item.url} alt={item.filename} />
-              </div>
-            ))}
-        </div>
-        {date && (
-          <span className="message__date">
-            <Time date={<Time date={new Date()} />} />
-          </span>
-        )}
-      </div>
-    </div>
-  </div>
-);
-
 Message.defaultProps = {
   user: {},
 };
