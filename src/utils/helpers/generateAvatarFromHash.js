c3f3a443b3e3b53e6f624b22c5a36c82a8e934b6
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-22 14:45:40 +0300

    styling

--- src/utils/helpers/generateAvatarFromHash.js
+++ src/utils/helpers/generateAvatarFromHash.js
@@ -16,7 +16,7 @@ export default (hash) => {
     .split("")
     .map((char) => getCorrectIndex(char.charCodeAt(0)));
   return {
-    color: tinycolor({ r, g, b }).lighten(10).saturate(10).toHexString(),
-    colorLighten: tinycolor({ r, g, b }).lighten(40).saturate(20).toHexString(),
+    color: tinycolor({ r, g, b }).lighten(15).saturate(10).toHexString(),
+    colorLighten: tinycolor({ r, g, b }).lighten(30).saturate(30).toHexString(),
   };
 };
