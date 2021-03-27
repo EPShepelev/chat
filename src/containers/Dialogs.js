4f1e537dfc100a3dbf55eb1de9014ea382f28e38
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-27 13:37:07 +0300

    add dialogs container

--- src/containers/Dialogs.js
+++ src/containers/Dialogs.js
@@ -1,3 +1,12 @@
 import React, { useState } from "react";
+import { Dialogs } from "components";
 
-const Dialogs = {};
+const Dialogs = ({ items }) => {
+  const [value, setValue] = useState("");
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+  return <Dialogs items={items} onSearch={onChangeInput} inputValue={value} />;
+};
+
+export default Dialogs;
