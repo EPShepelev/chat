c8376d8ceed4a9a8dfdad6e7709835239c626a44
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-28 08:31:54 +0300

    fixed class name

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -6,7 +6,7 @@ import { Input } from "antd";
 
 const Dialogs = ({ items, userId, onSearch }) => (
   <div className="dialogs">
-    <div className="dialogs-search">
+    <div className="dialogs__search">
       <Input.Search
         placeholder="Поиск среди контактов"
         onSearch={(value) => console.log(value)}
