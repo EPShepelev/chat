6af986aa98c6111ef9a521bd41a0dcb6002f7b5a
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-08 13:00:36 +0300

    modified index.js - add provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
