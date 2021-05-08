dcdd52ffbefb89f99ca5ae34542adc694d5a2a8e
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-08 17:10:16 +0300

    add compose

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
