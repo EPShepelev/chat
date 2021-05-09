fbc8b5104164beb68096933542026cdaa58b0872
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-09 12:32:23 +0300

    create dialog action

--- frontend/src/redux/actions/dialogs.js
+++ frontend/src/redux/actions/dialogs.js
@@ -1,1 +1,13 @@
 import { dialogsApi } from "utils/api";
+
+const actions = {
+  setDialogs: (items) => ({
+    type: "DIALOGS:SET_ITEMS",
+    payload: items,
+  }),
+  fetchDialogs: () => (dispatch) => {
+    dialogsApi.getAll().then(({ data }) => {
+      dispatch(actions.setDialogs(data));
+    });
+  },
+};
