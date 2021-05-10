7a2712ee321494570eac189dd6d70485fd4a411f
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-10 14:20:01 +0300

    fixed provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,11 @@ import store from "redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
+    <Provider store={store}>
+      <Router>
         <App />
-      </Provider>
-    </Router>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
