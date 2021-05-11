3111bedc4931987ce3238e7b3fc48917a050bb28
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-11 15:52:43 +0300

    create messages container

---
+++ frontend/src/containers/Messages.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Dialogs as BaseDialogs } from "components";
+import { dialogsActions } from "redux/actions";
+
+const Dialogs = ({ fetchDialogs, setCurrentDialog, items, userId }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [filtered, setFilteredItems] = useState(Array.from(items));
+
+  const onChangeInput = (value) => {
+    setFilteredItems(
+      items.filter(
+        (dialog) =>
+          dialog.user.fullname.toLowerCase().indexOf(value.toLowerCase()) >= 0
+      )
+    );
+    setInputValue(value);
+  };
+
+  useEffect(() => {
+    if (!items.length) {
+      fetchDialogs();
+    } else {
+      setFilteredItems(items);
+    }
+  }, [items]);
+
+  return (
+    <BaseDialogs
+      uerId={userId}
+      items={filtered}
+      onSearch={onChangeInput}
+      inputValue={inputValue}
+      onSelectDialog={setCurrentDialog}
+    />
+  );
+};
+
+export default connect(({ dialogs }) => dialogs, dialogsActions)(Dialogs);
