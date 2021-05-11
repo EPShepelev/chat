d8839d5e8657328fa1bd8a99c870c8810f72ac7c
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-11 15:29:18 +0300

    add on select dialog prop; fix

--- frontend/src/components/DialogItem/index.js
+++ frontend/src/components/DialogItem/index.js
@@ -13,11 +13,20 @@ const getMessageTime = (created_at) => {
   }
 };
 
-const DialogItem = ({ user, unread, created_at, isMe, text }) => (
+const DialogItem = ({
+  _id,
+  user,
+  unread,
+  created_at,
+  isMe,
+  text,
+  onSelect,
+}) => (
   <div
     className={classNames("dialogs__item", {
       "dialogs__item--online": user.isOnline,
     })}
+    onClick={onSelect.bind(this, _id)}
   >
     <div className="dialogs__item-avatar">
       <Avatar user={user} />
