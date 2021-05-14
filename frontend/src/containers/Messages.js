0974352f80ceada45a147d2a15977e76ff84c769
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-14 15:51:57 +0300

    modifed messages container

--- frontend/src/containers/Messages.js
+++ frontend/src/containers/Messages.js
@@ -3,27 +3,15 @@ import { connect } from "react-redux";
 import { Messages as BaseMessages } from "components";
 import { messagesActions } from "redux/actions";
 
-class Dialogs extends React.Component {
-  componentDidUpdate(prevProps) {
-    const { fetchMessages, currentDialogId } = this.props;
-    if (prevProps.currentDialogId !== this.props.currentDialogId) {
+const Dialogs = ({ currentDialogId, fetchMessages, items }) => {
+  useEffect(() => {
+    if (currentDialogId) {
       fetchMessages(currentDialogId);
     }
-  }
+  }, [currentDialogId]);
 
-  render() {
-    const { items } = this.props;
-    return <BaseMessages items={items} />;
-  }
-}
-
-// const Dialogs = ({ currentDialogId, fetchMessages, items }) => {
-//   useEffect(() => {
-//     fetchMessages(currentDialogId);
-//   }, [currentDialogId]);
-
-//   return <BaseMessages items={items} />;
-// };
+  return <BaseMessages items={items} />;
+};
 
 export default connect(
   ({ dialogs, messages }) => ({
