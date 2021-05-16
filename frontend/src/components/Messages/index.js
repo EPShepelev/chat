47a52e89e6f227c469a20e294eb0c0ae8959070a
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-16 10:25:46 +0300

    connect classnames for messages component

--- frontend/src/components/Messages/index.js
+++ frontend/src/components/Messages/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Empty, Spin } from "antd";
 import { Message } from "components";
+import classNames from "classnames";
 
 const Messages = ({ isLoading, items }) => {
   return (
-    <div className="messages">
+    <div className={classNames("messages", { "messages--loading": isLoading })}>
       {isLoading && !items ? (
         <Spin tip="Загрузка сообщений..." size="large"></Spin>
       ) : items ? (
