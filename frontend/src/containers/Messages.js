e6525a876f62e393b39fdf678a8b5966f5a9c822
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-16 09:37:42 +0300

    add is loading prop to messages container

--- frontend/src/containers/Messages.js
+++ frontend/src/containers/Messages.js
@@ -3,20 +3,21 @@ import { connect } from "react-redux";
 import { Messages as BaseMessages } from "components";
 import { messagesActions } from "redux/actions";
 
-const Dialogs = ({ currentDialogId, fetchMessages, items }) => {
+const Dialogs = ({ currentDialogId, fetchMessages, items, isLoading }) => {
   useEffect(() => {
     if (currentDialogId) {
       fetchMessages(currentDialogId);
     }
   }, [currentDialogId]);
 
-  return <BaseMessages items={items} />;
+  return <BaseMessages items={items} isLoading={isLoading} />;
 };
 
 export default connect(
   ({ dialogs, messages }) => ({
     currentDialogId: dialogs.currentDialogId,
     items: messages.item,
+    isLoading: messages.isLoading,
   }),
   messagesActions
 )(Dialogs);
