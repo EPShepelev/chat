9f65525c8f6e3000918274da3d84efbf4ab997b1
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-05-16 09:39:54 +0300

    deleted alert component

--- frontend/src/components/Messages/index.js
+++ frontend/src/components/Messages/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Empty, Spin, Alert } from "antd";
+import { Empty, Spin } from "antd";
 import { Message } from "components";
 
 const Messages = ({ isLoading, items }) => {
   return (
     <div className="messages">
       {isLoading && !items ? (
-        <Spin tip="Loading...">
-          <Alert
-            message="Alert message title"
-            description="Further details about the context of this alert."
-            type="info"
-          />
-        </Spin>
+        <Spin tip="Loading..."></Spin>
       ) : items ? (
         items.map((item) => <Message {...item} />)
       ) : (
