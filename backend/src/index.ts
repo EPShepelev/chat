9dde7f33f43ab9e88650705bfe4483a035be1d1d
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-21 14:01:50 +0300

    add mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (_: any, res: any) => {
   res.send("Hello World!");
