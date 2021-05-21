1f1552e037f60a9517bfc52641f558c8e145f2ca
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-21 14:05:31 +0300

    create schema.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/test", {
+mongoose.connect("mongodb://localhost:27017/chat", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
