20aca729574a964bb56f4007ca3c91d581903f71
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-21 14:13:42 +0300

    create user schema field

---
+++ backend/src/schemas/User.js
@@ -0,0 +1,14 @@
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: String,
+    avatar: String,
+    fullname: String,
+    password: String,
+    confirmed: Boolean,
+    confirm_hash: String,
+    last_seen: Date,
+  },
+  {}
+);
