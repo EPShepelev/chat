34c80a7e052f268994559fefd09c1282dccdbc7d
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-22 16:12:08 +0300

    modifed index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 import express from "express";
 
+import User from "./schemas/User";
+
 const app = express();
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/chat", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 app.get("/", (_: any, res: any) => {
   res.send("Hello World!");
+  const user = new User({ email: "hello@email.com", fullname: "Test User" });
+  user.save().then(() => console.log("User created!"));
 });
 
 app.listen(port, () => {
