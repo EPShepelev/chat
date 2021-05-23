815523c027998de89c822d769b3114a448363595
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-23 20:11:06 +0300

    modifed index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,10 +10,14 @@ mongoose.connect("mongodb://localhost:27017/chat", {
   useNewUrlParser: true,
 });
 
-app.get("/", (_: any, res: any) => {
-  res.send("Hello World!");
+app.get("/create", (_: any, res: any) => {
   const user = new User({ email: "hello@email.com", fullname: "Test User" });
-  user.save().then(() => console.log("User created!"));
+  user.save().then((err: any, obj: any) => {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(obj);
+  });
 });
 
 app.listen(port, () => {
