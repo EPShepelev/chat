82f444d37d1e62705b2bcc96d0e545f2a8ec2ee2
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-25 11:49:00 +0300

    edited user controller

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,14 @@
 import express from "express";
 
+import { UserModel } from "../schemas";
+
 class UserController {
-  index(req: express.Request, res: express.Response) {}
+  index(req: express.Request, res: express.Response) {
+    const id: string = req.params.id;
+    UserModel.findById(id).then((user) => {
+      res.json(user);
+    });
+  }
 }
+
+export default UserController;
