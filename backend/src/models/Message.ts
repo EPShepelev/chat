8db91d4acd7b9991c4fc13b3b888b9bdbefe3b5d
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-05-28 13:25:37 +0300

    create message model file

---
+++ backend/src/models/Message.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { isEmail } from "validator";
+
+export interface IDialog extends Document {
+  author: {
+    type: Schema.Types.ObjectId;
+    ref: String;
+    require: true;
+  };
+  partner: {
+    type: Schema.Types.ObjectId;
+    ref: String;
+    require: true;
+  };
+  lastMessages: {
+    type: Schema.Types.ObjectId;
+    ref: String;
+  };
+}
+
+const DialogSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    partner: { type: Schema.Types.ObjectId, ref: "User" },
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const DialogModel = mongoose.model<IDialog>("Dialog", DialogSchema);
+
+export default DialogModel;
