12621670566634f994b8b77b76278041d9fda505
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-02 15:45:17 +0300

    create message controller(file)

---
+++ backend/src/controllers/MessageController.ts
@@ -0,0 +1,55 @@
+import express from "express";
+
+import { DialogModel } from "../models";
+
+class DialogController {
+  index(req: express.Request, res: express.Response) {
+    const authorId: string = req.params.id;
+
+    DialogModel.find({ author: authorId })
+      .populate(["author", "partner"])
+      .exec(function (err, dialogs) {
+        if (err) {
+          return res.status(404).json({
+            message: "Dialogs not found",
+          });
+        }
+        return res.json(dialogs);
+      });
+  }
+
+  create(req: express.Request, res: express.Response) {
+    const postData = {
+      author: req.body.author,
+      partner: req.body.partner,
+    };
+    const dialog = new DialogModel(postData);
+    dialog
+      .save()
+      .then((obj: any) => {
+        res.json(obj);
+      })
+      .catch((reason) => {
+        res.json({ reason });
+      });
+  }
+
+  delete(req: express.Request, res: express.Response) {
+    const id: string = req.params.id;
+    DialogModel.findOneAndRemove({ _id: id })
+      .then((dialog) => {
+        if (dialog) {
+          res.json({
+            message: "Dialog deleted",
+          });
+        }
+      })
+      .catch(() => {
+        res.json({
+          message: "Dialog not found",
+        });
+      });
+  }
+}
+
+export default DialogController;
