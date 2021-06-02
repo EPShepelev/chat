14d1e78173ee99458f8a571ded5eb48d0dbd1665
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-02 16:02:46 +0300

    create index method in message controller

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -1,55 +1,54 @@
 import express from "express";
 
-import { DialogModel } from "../models";
+import { MessageModel } from "../models";
 
-class DialogController {
+class MessageController {
   index(req: express.Request, res: express.Response) {
-    const authorId: string = req.params.id;
-
-    DialogModel.find({ author: authorId })
-      .populate(["author", "partner"])
-      .exec(function (err, dialogs) {
+    const dialogId: string = req.params.dialogId;
+    MessageModel.find({ dialog: dialogId })
+      .populate(["dialog"])
+      .exec(function (err, messages) {
         if (err) {
           return res.status(404).json({
-            message: "Dialogs not found",
+            message: "Messages not found",
           });
         }
-        return res.json(dialogs);
+        return res.json(messages);
       });
   }
 
-  create(req: express.Request, res: express.Response) {
-    const postData = {
-      author: req.body.author,
-      partner: req.body.partner,
-    };
-    const dialog = new DialogModel(postData);
-    dialog
-      .save()
-      .then((obj: any) => {
-        res.json(obj);
-      })
-      .catch((reason) => {
-        res.json({ reason });
-      });
-  }
+  // create(req: express.Request, res: express.Response) {
+  //   const postData = {
+  //     author: req.body.author,
+  //     partner: req.body.partner,
+  //   };
+  //   const dialog = new DialogModel(postData);
+  //   dialog
+  //     .save()
+  //     .then((obj: any) => {
+  //       res.json(obj);
+  //     })
+  //     .catch((reason) => {
+  //       res.json({ reason });
+  //     });
+  // }
 
-  delete(req: express.Request, res: express.Response) {
-    const id: string = req.params.id;
-    DialogModel.findOneAndRemove({ _id: id })
-      .then((dialog) => {
-        if (dialog) {
-          res.json({
-            message: "Dialog deleted",
-          });
-        }
-      })
-      .catch(() => {
-        res.json({
-          message: "Dialog not found",
-        });
-      });
-  }
+  // delete(req: express.Request, res: express.Response) {
+  //   const id: string = req.params.id;
+  //   DialogModel.findOneAndRemove({ _id: id })
+  //     .then((dialog) => {
+  //       if (dialog) {
+  //         res.json({
+  //           message: "Dialog deleted",
+  //         });
+  //       }
+  //     })
+  //     .catch(() => {
+  //       res.json({
+  //         message: "Dialog not found",
+  //       });
+  //     });
+  // }
 }
 
 export default DialogController;
