885d63d274f51395f4b3294dbf2311b0163ab1ab
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-04 08:53:16 +0300

    add user file to message

--- backend/src/models/Message.ts
+++ backend/src/models/Message.ts
@@ -21,6 +21,7 @@ const MessageSchema = new Schema(
   {
     text: { type: String, require: Boolean },
     dialog: { type: Schema.Types.ObjectId, ref: "Dialog", require: true },
+    user: { type: Schema.Types.ObjectId, require: true },
     unread: {
       type: boolean,
       default: false,
