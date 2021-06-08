7d33fd905de65c41dab742ca91fad2a90b270c33
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-08 15:48:52 +0300

    fix req and res parameters

--- backend/src/middlewares/updateLastSeen.ts
+++ backend/src/middlewares/updateLastSeen.ts
@@ -1,6 +1,6 @@
 import { UserModel } from "../models";
 
-export default (req, res, next) => {
+export default (req: express.Request, res: express.Response, next) => {
   UserModel.updateOne({ _id: req.user._id, last_seen: new Date() });
   next();
 };
