e85c831e76ab150ff313668b31ed792bb5ae2118
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-11 08:33:11 +0300

    add details age settings

--- backend/src/utils/createJWTToken.js
+++ backend/src/utils/createJWTToken.js
@@ -4,4 +4,7 @@ export default (details) => {
   if (typeof details !== "object") {
     details = {};
   }
+  if (!details.maxAge || typeof details.maxAge !== "number") {
+    details.maxAge = 3600;
+  }
 };
