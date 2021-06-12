ef7def8b44b9b6adca4915095fa6f6b5dee4b1dc
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-12 08:32:25 +0300

    delete session age func

--- backend/src/utils/createJWTToken.js
+++ backend/src/utils/createJWTToken.js
@@ -1,11 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export default (details) => {
-  if (typeof details !== "object") {
-    details = {};
-  }
-  if (!details.maxAge || typeof details.maxAge !== "number") {
-    details.maxAge = 3600;
+  
   }
 
   // details.sessionData = _.reduce(
