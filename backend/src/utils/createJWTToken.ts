292b5b890f11ad84851a0074701e8b56fbcb9bf4
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-16 14:48:28 +0300

    fix create token func

--- backend/src/utils/createJWTToken.ts
+++ backend/src/utils/createJWTToken.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { reduce } from "lodash";
-import { IUser } from "../models/User";
+
+interface ILoginData
 
 export default (user: any) => {
   let token = jwt.sign(
