cd31f2fb95f322fbf8906c66c98af5ba3ac14be9
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-18 15:17:36 +0300

    fix login to email

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -54,7 +54,7 @@ class UserController {
 
   login(req: express.Request, res: express.Response) {
     const postData = {
-      email: req.body.login,
+      email: req.body.email,
       password: req.body.password,
     };
 
