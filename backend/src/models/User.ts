9e463309c7c246d95703ceb49992bdec61dd5c02
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-19 22:48:45 +0300

    change iuser fields

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 import { isEmail } from "validator";
 
 export interface IUser extends Document {
-  email: string;
-  fullname: string;
-  password: string;
-  confirmed: boolean;
+  email?: string;
+  fullname?: string;
+  password?: string;
+  confirmed?: boolean;
   avatar?: string;
   confirm_hash?: string;
   last_seen?: Date;
