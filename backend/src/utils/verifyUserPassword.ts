5ebc98800452375cf32cb7aaa390341d0a817a1b
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-06-20 07:24:31 +0300

    add promise to verify possword func

--- backend/src/utils/verifyUserPassword.ts
+++ backend/src/utils/verifyUserPassword.ts
@@ -1,12 +1,14 @@
 import bcrypt from "bcrypt";
 
 export default (password: string, handleError: any) => {
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return handleError(err);
-    bcrypt.hash(user.password, salt, function (err, hash) {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
       if (err) return handleError(err);
-      user.password = hash;
-      next();
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return handleError(err);
+        user.password = hash;
+        next();
+      });
     });
   });
 };
