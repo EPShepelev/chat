982ef0b03029f395c9bb6180574116792b7ffe72
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-01 23:27:54 +0300

    replace routes

--- backend/src/core/routes.js
+++ backend/src/core/routes.js
@@ -1,1 +1,24 @@
-export default (app) => {};
+import { updateLastSeen, checkAuth } from "./middlewares";
+import { loginValidation } from "./utils/validations";
+
+export default (app) => {
+  app.use(bodyParser.json());
+  app.use(updateLastSeen);
+  app.use(checkAuth);
+
+  const port = 3000;
+
+  app.get("/user/me", UserController.getMe);
+  app.get("/user/:id", UserController.show);
+  app.delete("/user/:id", UserController.delete);
+  app.post("/user/registration", UserController.create);
+  app.post("/user/login", loginValidation, UserController.login);
+
+  app.get("/dialogs", DialogController.index);
+  app.post("/dialogs", DialogController.create);
+  app.delete("/dialogs/:id", DialogController.delete);
+
+  app.get("/messages", MessageController.index);
+  app.post("/messages", MessageController.create);
+  app.delete("/messages/:id", MessageController.delete);
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,33 +12,10 @@ import {
 
 import "./core/db";
 
-import { updateLastSeen, checkAuth } from "./middlewares";
-import { loginValidation } from "./utils/validations";
-
 const app = express();
 const http = creatServer(app);
 const io = socket(http);
 dotenv.config();
-
-app.use(bodyParser.json());
-app.use(updateLastSeen);
-app.use(checkAuth);
-
-const port = 3000;
-
-app.get("/user/me", UserController.getMe);
-app.get("/user/:id", UserController.show);
-app.delete("/user/:id", UserController.delete);
-app.post("/user/registration", UserController.create);
-app.post("/user/login", loginValidation, UserController.login);
-
-app.get("/dialogs", DialogController.index);
-app.post("/dialogs", DialogController.create);
-app.delete("/dialogs/:id", DialogController.delete);
-
-app.get("/messages", MessageController.index);
-app.post("/messages", MessageController.create);
-app.delete("/messages/:id", MessageController.delete);
 
 io.on("connection", function (socket: any) {
   console.log("CONNECTED!");
