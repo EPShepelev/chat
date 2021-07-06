311067e783e700e01000eabf34602590221ef2f7
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-06 11:30:43 +0300

    refactored index and socket

---
+++ backend/src/core/socket.ts
@@ -0,0 +1,8 @@
+import socket from "socket.io";
+import http from "http";
+
+export default (http: http.Server) => {
+  const io = socket(http);
+  io.on("connection", function (socket: any) {});
+  return io;
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,19 @@
 import express from "express";
-import socket from "socket.io";
 import { creatServer } from "http";
 import dotenv from "dotenv";
 
 import "./core/db";
 import createRoutes from "./core/routes";
+import createSocket from "./core/socket";
 
 const app = express();
 const http = creatServer(app);
-const io = socket(http);
+const io = createSocket(http);
 
 dotenv.config();
 
 createRoutes(app, io);
-
-io.on("connection", function (socket: any) {});
+createSocket(io);
 
 http.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
