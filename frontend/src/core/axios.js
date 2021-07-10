484de8c31c30bd07fed79dbfba8795d8bd20cb76
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-10 10:07:52 +0300

    add window axios

--- frontend/src/core/axios.js
+++ frontend/src/core/axios.js
@@ -3,4 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = window.location.origin;
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
+window.axios = axios;
+
 export default axios;
