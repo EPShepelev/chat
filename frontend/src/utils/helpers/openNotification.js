2890bc5dab666f5f51a8a1153fc6df55f7ed51cf
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-12 08:02:23 +0300

    create notification from antd

--- frontend/src/utils/helpers/openNotification.js
+++ frontend/src/utils/helpers/openNotification.js
@@ -1,1 +1,11 @@
 import { notification } from "antd";
+
+export default (text, type="info") => {
+  notification.open({
+    message: 'Notification Title',
+    description:
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    onClick: () => {
+      console.log('Notification Clicked!');
+    },
+}
