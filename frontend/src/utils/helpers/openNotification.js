b7fc72d36e69be8ad7393f4a167fcbbc8832e744
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-12 08:18:56 +0300

    add duration for notification

--- frontend/src/utils/helpers/openNotification.js
+++ frontend/src/utils/helpers/openNotification.js
@@ -1,8 +1,9 @@
 import { notification } from "antd";
 
-export default ({title, text, type="info"}) => {
+export default ({title, text, type="info", duration = 2}) => {
   notification.open({
     message: title,
     description:
     text,
+    duration: duration
 }
