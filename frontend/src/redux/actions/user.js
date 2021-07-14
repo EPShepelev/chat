6878b2215b92c3913616ff57c67106d8b09d921f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-14 07:57:23 +0300

    add dispath to user action

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -1,12 +1,14 @@
 import { userApi } from "utils/api";
 
-const actions = {
+const Actions = {
   setUserData: (data) => ({
     type: "USER:SET_DATA",
     payload: data,
   }),
   fetchUserLogin: (postData) => (dispatch) => {
-    return userApi.login(postData).then(({ data }) => {});
+    return userApi.login(postData).then(({ data }) => {
+      dispatch(Actions.setUserData(data));
+    });
   },
 };
 
