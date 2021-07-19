6b036daf5150a7b7ac2fff414ab964153ecd2360
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-19 23:02:19 +0300

    uncomment axios link

--- frontend/src/core/axios.js
+++ frontend/src/core/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = window.location.origin;
 axios.defaults.headers.common["token"] = window.token;
 
-// window.axios = axios;
+window.axios = axios;
 
 export default axios;
