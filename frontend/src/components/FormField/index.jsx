8a5d2fae48cb502afdfffdf29761b50f9fee3cd3
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-24 17:41:41 +0300

    add form field default conde

---
+++ frontend/src/components/FormField/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+import store from "./store";
+import * as actionTypes from "./store/actionTypes";
+
+const FormField = () => {
+  return <div></div>;
+};
+
+export default FormField;
