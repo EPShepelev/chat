39f931f7033b45a18975e7a78e1a3d653b52a3dd
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-27 21:48:36 +0300

    modifed registration ts

--- backend/src/utils/validations/registration.ts
+++ backend/src/utils/validations/registration.ts
@@ -1,6 +1,7 @@
-import { check, validationResult } from "express-validator";
+import { check } from "express-validator";
 
 export default [
   check("username").isEmail(),
+  check("full_name").isLength({ min: 5 }),
   check("password").isLength({ min: 5 }),
 ];
