51dae04b46e5c30e98bb3ae93da2435a25fcf7a1
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-28 07:37:34 +0300

    add regstration request

--- frontend/src/utils/api/user.js
+++ frontend/src/utils/api/user.js
@@ -2,5 +2,6 @@ import { axios } from "core";
 
 export default {
   login: (postData) => axios.post("./user/login", postData),
+  register: (postData) => axios.post("./user/signup", postData),
   getMe: () => axios.get("./user/me"),
 };
