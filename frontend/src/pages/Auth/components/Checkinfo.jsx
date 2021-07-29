3d70f0a64c8ae2394df771fc4fd0c660c3eb2126
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-29 07:44:21 +0300

    add default code

--- frontend/src/pages/Auth/components/Checkinfo.jsx
+++ frontend/src/pages/Auth/components/Checkinfo.jsx
@@ -3,23 +3,8 @@ import PropTypes from "prop-types";
 import store from "./store";
 import * as actionTypes from "./store/actionTypes";
 
-export class Checkinfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    store.subscribe(this.handleStoreChange);
-  }
+const CheckEmailInfo = () => {
+  return <div></div>;
+};
 
-  render() {
-    return <Fragment></Fragment>;
-  }
-
-  handleStoreChange() {
-    this.setState(store.getState());
-  }
-}
-
-Checkinfo.propTypes = {};
-
-export default Checkinfo;
+export default CheckEmailInfo;
