10a8f9e0b80ccf0c0e78dcdf253d23f316021595
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-07-30 07:46:13 +0300

    fix

--- frontend/src/pages/Auth/components/Checkinfo.jsx
+++ frontend/src/pages/Auth/components/Checkinfo.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Block, Result } from "antd";
 
-const { Title } = Typography;
-
 const CheckEmailInfo = () => {
   return (
     <div>
-    <Block>
-    <Result status="success" title="Готово!" subTitle="Регистрация прошла успешно. Ссылка для подтверждения регистрации отправлена на e-mail" />
-    </Block>
-  </div>;
+      <Block>
+        <Result
+          status="success"
+          title="Готово!"
+          subTitle="Регистрация прошла успешно. Ссылка для подтверждения регистрации отправлена на e-mail"
+        />
+      </Block>
+    </div>
   );
 };
 
 export default CheckEmailInfo;
-
-
