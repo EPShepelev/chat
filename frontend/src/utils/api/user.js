452bf3a63050d8faf31813b7018bcb567caf78ce
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-01 17:18:49 +0300

    modifed user api

--- frontend/src/utils/api/user.js
+++ frontend/src/utils/api/user.js
@@ -3,5 +3,6 @@ import { axios } from "core";
 export default {
   signin: (postData) => axios.post("./user/signin", postData),
   signup: (postData) => axios.post("./user/signup", postData),
+  verifyHash: (hash) => axios.get("./user/verify?hash=" + hash),
   getMe: () => axios.get("./user/me"),
 };
