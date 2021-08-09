514ca2454c3c4b0f4fcff3ec322fbadc9832640e
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-09 07:54:20 +0300

    add switch tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import { Auth, Home } from "pages";
 
@@ -8,22 +8,24 @@ const App = (props) => {
   const { isAuth } = props;
   return (
     <div className="wrapper">
-      <Route
-        exact
-        path={["/signin", "/signup", "/signup/verify"]}
-        component={Auth}
-      />
+      <Switch>
+        <Route
+          exact
+          path={["/signin", "/signup", "/signup/verify"]}
+          component={Auth}
+        />
 
-      <Route
-        exact
-        path={["/", "dialog/:id"]}
-        component={Auth}
-        render={() => {
-          {
-            isAuth ? <Home /> : <Redirect to="/signin" />;
-          }
-        }}
-      />
+        <Route
+          exact
+          path={["/", "dialog/:id"]}
+          component={Auth}
+          render={() => {
+            {
+              isAuth ? <Home /> : <Redirect to="/signin" />;
+            }
+          }}
+        />
+      </Switch>
     </div>
   );
 };
