73eada93c92de69614f808fdf4bffe92420b259c
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-11 07:46:26 +0300

    refactoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "redux/store";
+import "core/socket";
+
 import App from "./App";
 import "./styles/index.scss";
 import "emoji-mart/css/emoji-mart.css";
-import { Provider } from "react-redux";
-import store from "redux/store";
-import "core/socket";
 
 ReactDOM.render(
   <React.StrictMode>
