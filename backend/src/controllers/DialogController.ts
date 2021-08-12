39fc23e06a1394c5eade5e8e58f8ad74d6e2d476
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-12 07:39:39 +0300

    fix

--- backend/src/controllers/DialogController.ts
+++ backend/src/controllers/DialogController.ts
@@ -47,7 +47,7 @@ class DialogController {
         .save()
         .then(() => {
           res.json(dialogObj);
-          this.io.emit("SERVER:DIALOG_CREATED", message);
+          this.io.emit("SERVER:DIALOG_CREATED");
         })
         .catch((reason) => {
           res.json({ reason });
