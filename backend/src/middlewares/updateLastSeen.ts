6f6010d122348539d10f03c98b7c05a048c7700e
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-18 22:18:52 +0300

    refactoring; add conditions

--- backend/src/middlewares/updateLastSeen.ts
+++ backend/src/middlewares/updateLastSeen.ts
@@ -6,13 +6,16 @@ export default (
   __: express.Response,
   next: express.NextFunction
 ) => {
-  UserModel.findOneAndUpdate(
-    {
-      _id: req.user._id,
-    },
-    { last_seen: new Date() },
-    { new: true },
-    () => {}
-  );
+  if (req.user) {
+    UserModel.findOneAndUpdate(
+      {
+        _id: req.user._id,
+      },
+      { last_seen: new Date() },
+      { new: true },
+      () => {}
+    );
+  }
+
   next();
 };
