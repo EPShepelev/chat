8479f76de90d305be988a71022fbb33dce784d47
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-22 14:23:34 +0300

    set fetch send message method

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -3,7 +3,7 @@ import { ChatInput } from "components";
 import {connect} from 'react-redux';
 import {messagesActions} from 'redux/actions';
 
-const ChatInput = () => {
+const ChatInput = ({fetchSendMessage}) => {
   return <ChatInput onSendMessage={}/>;
 };
 
