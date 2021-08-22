69d0286924af4d5e24bc36e958ff497ea194d0d9
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-22 14:24:39 +0300

    refactoring

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -1,10 +1,10 @@
 import React, { Component, Fragment } from "react";
-import { ChatInput } from "components";
-import {connect} from 'react-redux';
-import {messagesActions} from 'redux/actions';
+import { ChatInput as ChatInputBase } from "components";
+import { connect } from "react-redux";
+import { messagesActions } from "redux/actions";
 
-const ChatInput = ({fetchSendMessage}) => {
-  return <ChatInput onSendMessage={}/>;
+const ChatInput = ({ fetchSendMessage }) => {
+  return <ChatInputBase onSendMessage={fetchSendMessage} />;
 };
 
 export default connect(null, messagesActions)(ChatInput);
