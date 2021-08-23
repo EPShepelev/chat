d1dda49f11f3bbc0ad7090774126ae22940fc884
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-23 07:46:44 +0300

    bind

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -4,7 +4,11 @@ import { connect } from "react-redux";
 import { messagesActions } from "redux/actions";
 
 const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
-  return <ChatInputBase onSendMessage={fetchSendMessage} />;
+  return (
+    <ChatInputBase
+      onSendMessage={fetchSendMessage.bind(this, currentDialogId)}
+    />
+  );
 };
 
 export default connect(({ dialogs }) => dialogs, messagesActions)(ChatInput);
