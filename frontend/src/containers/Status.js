107f54311e9a59659d291886e87d520c4909ed9c
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-08-28 22:07:27 +0300

    refactoring

--- frontend/src/containers/Status.js
+++ frontend/src/containers/Status.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Status as StatusBase } from "components";
 import { connect } from "react-redux";
 
-const Status = ({ currentDialogId, items }) => {
+const Status = ({ currentDialogId, user, items }) => {
   const currentDialogObj = items.filter(
     (dialog) => dialog._id === currentDialogId
   )[0];
+  let online = false;
+  if (dialog.author._id === user._id) {
+    online = dialog.partner.isOnline;
+  }
   return <StatusBase online={online} />;
 };
 
