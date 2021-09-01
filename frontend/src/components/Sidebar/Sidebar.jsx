f51dffede3d327916d5a29a3470f4a4f3c73e626
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-01 22:16:08 +0300

    replaced sidebar layout

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="chat__sidebar">
+      <div className="chat__sidebar-header">
+        <TeamOutlined />
+        <div>
+          <span>Список диалогов</span>
+        </div>
+        <Button type="link" shape="circle" icon={<FormOutlined />} />
+      </div>
+
+      <div className="chat__sidebar-dialogs">
+        <Dialogs userId={0} />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -10,19 +10,6 @@ import "./Home.scss";
 
 const Home = () => (
   <section className="chat">
-    <div className="chat__sidebar">
-      <div className="chat__sidebar-header">
-        <TeamOutlined />
-        <div>
-          <span>Список диалогов</span>
-        </div>
-        <Button type="link" shape="circle" icon={<FormOutlined />} />
-      </div>
-
-      <div className="chat__sidebar-dialogs">
-        <Dialogs userId={0} />
-      </div>
-    </div>
     <div className="chat__dialog">
       <div className="chat__dialog-header">
         <div />
