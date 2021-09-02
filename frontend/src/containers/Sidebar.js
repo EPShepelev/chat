37152f8ae963d0ab2862abc9b84bfbb922b1c4c8
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-02 07:50:35 +0300

    refactoring

--- frontend/src/containers/Sidebar.js
+++ frontend/src/containers/Sidebar.js
@@ -3,8 +3,4 @@ import { connect } from "react-redux";
 
 import { Sidebar } from "components";
 
-const SidebarContainer = ({ userId }) => {
-  return <Sidebar userId={uder._id} />;
-};
-
-export default connect(({ user }) => user.data)(SidebarContainer);
+export default connect(({ user }) => user.data)(Sidebar);
