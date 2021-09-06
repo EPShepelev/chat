14796c0d34d7fef37d53236456e2ddf75a43f850
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-06 22:36:29 +0300

    refactoring

--- frontend/src/containers/Sidebar.js
+++ frontend/src/containers/Sidebar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button, Select } from "antd";
 import { connect } from "react-redux";
 
 import { Sidebar } from "components";
