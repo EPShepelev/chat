c7d8c466d977344ec420e17bb6aa66cb163deb58
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-11 20:45:52 +0300

    add route

--- backend/src/core/routes.ts
+++ backend/src/core/routes.ts
@@ -21,6 +21,7 @@ const createRoutes = (app: express.Express, io: socket.Server) => {
   app.get("/user/verify", UserController.verify);
   app.post("/user/signup", registerValidation, UserController.create);
   app.post("/user/signin", loginValidation, UserController.login);
+  app.post("/user/find", UserController.findUsers);
   app.get("/user/:id", UserController.show);
   app.delete("/user/:id", UserController.delete);
 
