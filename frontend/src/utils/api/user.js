9b406025ee11207fdd3174ecc33ddc4fb6d83c94
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-12 21:39:29 +0300

    fix

--- frontend/src/utils/api/user.js
+++ frontend/src/utils/api/user.js
@@ -5,5 +5,5 @@ export default {
   signup: (postData) => axios.post("./user/signup", postData),
   verifyHash: (hash) => axios.get("./user/verify?hash=" + hash),
   getMe: () => axios.get("./user/me"),
-  findUsers: (name) => axios.get("./user/find"),
+  findUsers: (query) => axios.get("./user/find?query=" + query),
 };
