647b62579c85892a088ace569b9665c3c9d2bfef
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-09-20 21:30:36 +0300

    added form

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -64,6 +64,22 @@ const Sidebar = ({
           </Button>,
         ]}
       >
+        <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
         <Select
           placeholder="Найти пользователя"
           value={inputValue}
