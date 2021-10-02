9b9965d34661124866dbd6027e7fd7c886b8eb54
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-02 21:38:55 +0300

    fix type

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -98,7 +98,7 @@ class MessageController {
                 message: err,
               });
             }
-            DialogModel.findById(dialogId, (err, dialog) => {
+            DialogModel.findById(dialogId, (err, dialog: any) => {
               if (err) {
                 res.status(500).json({
                   status: "error",
