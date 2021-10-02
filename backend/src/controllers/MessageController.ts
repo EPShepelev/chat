c8c927ee7893f3c8f222021975aecd0a29af26ae
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-02 21:38:26 +0300

    modifd

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -105,6 +105,7 @@ class MessageController {
                   message: err,
                 });
               }
+              dialog.lastMessage = lastMessage;
             });
           }
         );
