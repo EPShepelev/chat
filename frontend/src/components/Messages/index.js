a6769bbc14f11e9452f95e1d8b0564892d98bbae
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-06 22:44:10 +0300

    temp addition

--- frontend/src/components/Messages/index.js
+++ frontend/src/components/Messages/index.js
@@ -19,7 +19,7 @@ const Messages = ({ onRemoveMessage, blockRef, isLoading, items, user }) => {
             <Message
               key={item._id}
               {...item}
-              isMe={user._id === item.user._id}
+              isMe={user && user._id === item.user._id}
               onRemoveMessage={onRemoveMessage.bind(this, item._id)}
             />
           ))
