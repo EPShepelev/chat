da96cad35b71a4d86f55befcf150ca21cfa5158c
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-06 22:45:32 +0300

    fix

--- frontend/src/containers/Status.js
+++ frontend/src/containers/Status.js
@@ -3,19 +3,19 @@ import { Status as StatusBase } from "components";
 import { connect } from "react-redux";
 
 const ChatInput = ({ currentDialogId, user, dialogs }) => {
-  if(!dialogs.length || ! currentDialogId){
-    return null
+  if (!dialogs.length || !currentDialogId) {
+    return null;
   }
   const currentDialogObj = items.filter(
     (dialog) => dialog._id === currentDialogId
   )[0];
   let partner = {};
-  if currentDialogObj.author._id === user._id) {
+  if (currentDialogObj.author._id === user._id) {
     partner = currentDialogObj.partner;
   } else {
     partner = currentDialogObj.author;
   }
-  return <StatusBase online={partner.isOnline} fullname = {partner.fullname}/>;
+  return <StatusBase online={partner.isOnline} fullname={partner.fullname} />;
 };
 
 export default connect(({ dialogs, user }) => ({
