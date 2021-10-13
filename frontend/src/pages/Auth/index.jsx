8452c9a4936d67562ad2431fe7858e0bb1d58577
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-13 21:45:55 +0300

    deleted unnesesary import

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
-import { WithRouter } from "react-router";
 
 import { LoginForm, RegistrationForm } from "modules";
 import CheckInfo from "./components/CheckEmailInfo";
