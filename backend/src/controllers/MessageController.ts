a74f680828c2a9a7cdc245b3c3ee2cbc7abb863d
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-17 09:46:07 +0300

    modified

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -12,6 +12,7 @@ class MessageController {
 
   index = (req: express.Request, res: express.Response) => {
     const dialogId: string = req.query.dialog;
+    const user = req.user;
     MessageModel.find({ dialog: dialogId })
       .populate(["dialog", "user"])
       .exec(function (err, messages) {
