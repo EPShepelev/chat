a34602d16c8f679e725fb24053ce4c7fa3c2b9fd
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-18 22:10:33 +0300

    create upload controller file

---
+++ backend/src/controllers/UploadController.ts
@@ -0,0 +1,159 @@
+import { validationResult } from "express-validator";
+import { nextTick } from "process";
+import { isObject } from "lodash";
+import express from "express";
+import bcrypt from "bcrypt";
+import socket from "socket.io";
+
+import { UserModel } from "../models";
+import { IUser } from "../models/User";
+import { createJWTToken } from "../utils";
+
+class UserController {
+  io: socket.Server;
+  constructor(io: socket.Server) {
+    this.io = io;
+  }
+
+  show = (req: express.Request, res: express.Response) => {
+    const id: string = req.params.id;
+    UserModel.findById(id, (err, user) => {
+      if (err || !user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+      res.json(user);
+    });
+  };
+
+  getMe = (req: any, res: express.Response, io: any) => {
+    const id: string = req.user._id;
+    UserModel.findById(id, (err, user: any) => {
+      if (err || !user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+      res.json(user);
+    });
+  };
+
+  findUsers = (req: any, res: express.Response, io: any) => {
+    const query: string = req.query.query;
+    UserModel.find()
+      .or([
+        { fullname: new RegExp(query, "i") },
+        { email: new RegExp(query, "i") },
+      ])
+      .then((users: any) => res.json(users))
+      .catch((err: any) => {
+        return res.status(404).json({
+          status: "error",
+          message: err,
+        });
+      });
+  };
+
+  delete = (req: express.Request, res: express.Response) => {
+    const id: string = req.params.id;
+    UserModel.findOneAndRemove({ _id: id })
+      .then((user) => {
+        if (user) {
+          res.json({
+            message: `User ${user.fullname} deleted`,
+          });
+        }
+      })
+      .catch(() => {
+        res.json({
+          message: "User not found",
+        });
+      });
+  };
+
+  create = (req: express.Request, res: express.Response) => {
+    const postData = {
+      email: req.body.email,
+      fullname: req.body.fullname,
+      password: req.body.password,
+    };
+
+    const user = new UserModel(postData);
+
+    user
+      .save()
+      .then((obj: any) => {
+        res.json(obj);
+      })
+      .catch((reason) => {
+        res.status(500).json({
+          status: "error",
+          message: reason,
+        });
+      });
+  };
+
+  verify = (req: express.Request, res: express.Response) => {
+    const hash = req.query.hash;
+    if (!hash) {
+      return res.status(422).json({ errors: "Invalid hash" });
+    }
+    UserModel.findOne({ confirm_hash: hash }, (err, user) => {
+      if (err || !user) {
+        return res.status(404).json({
+          status: "error",
+          message: "Hash not found",
+        });
+      }
+
+      user.confirmed = true;
+      user.save((err) => {
+        if (err) {
+          return res.status(404).json({
+            status: "error",
+            message: err,
+          });
+        }
+        res.json({
+          status: "success",
+          message: "Аккаунт подтвержден!",
+        });
+      });
+    });
+  };
+
+  login = (req: express.Request, res: express.Response) => {
+    const postData = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    UserModel.findOne({ email: postData.email }, (err, user: IUser) => {
+      if (err) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      if (bcrypt.compareSync(postData.password, user.password)) {
+        const token = createJWTToken(user);
+        res.json({
+          status: "success",
+          token,
+        });
+      } else {
+        res.status(403).json({
+          status: "error",
+          message: "invalid password or email",
+        });
+      }
+    });
+  };
+}
+
+export default UserController;
