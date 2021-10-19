bc8632041d7ffe7ad31117587f38bf46c818515f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-19 23:31:24 +0300

    modifed upload file controller

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -1,5 +1,7 @@
 import { UploadFileModel } from "../models";
 
-class UserController {}
+class UserController {
+  create = (req: express.Request, res: Express.Response) => {};
+}
 
 export default UserController;
