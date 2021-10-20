6511cb8f158df201b1bc16a314d833d3151280e1
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-20 23:19:28 +0300

    export controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as UserCtr } from "./UserController";
 export { default as DialogCtr } from "./DialogController";
 export { default as MessageCtr } from "./MessageController";
+export { default as UploadCtr } from "./UploadController";
