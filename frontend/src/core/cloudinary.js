35fc526997b6e20873a89c94a1a90a720363ce8c
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-20 23:45:38 +0300

    create config

--- frontend/src/core/cloudinary.js
+++ frontend/src/core/cloudinary.js
@@ -1,3 +1,7 @@
 const cloudinary = require("cloudinary");
 
-cloudinary.config({});
+cloudinary.config({
+  cloud_name: config.cloudinary.name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret,
+});
