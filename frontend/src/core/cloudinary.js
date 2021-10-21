e173b08c3158535dd5ce634655470f516aff834f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-21 23:45:12 +0300

    deleted cloud from front

--- frontend/src/core/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require("cloudinary");
-
-cloudinary.config({
-  cloud_name: config.cloudinary.name,
-  api_key: config.cloudinary.api_key,
-  api_secret: config.cloudinary.api_secret,
-});
