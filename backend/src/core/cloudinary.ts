80f41c2b90630583ea7663ad21edeeae8f26a832
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-22 21:44:10 +0300

    add key

--- backend/src/core/cloudinary.ts
+++ backend/src/core/cloudinary.ts
@@ -2,6 +2,6 @@ const cloudinary = require("cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: config.cloudinary.api_key,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: config.cloudinary.api_secret,
 });
