ffae55730f3a22637a9135a2976ee37d7c8f659f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-24 22:11:52 +0300

    create delete method

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -2,6 +2,7 @@ import { UploadFileModel } from "../models";
 
 class UserController {
   create = (req: express.Request, res: Express.Response) => {};
+  delete = () => {};
 }
 
 export default UserController;
