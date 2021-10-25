613f3f30870f2ebfbf370cd14da8460088a91228
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-25 22:40:16 +0300

    modifed upload ctrl

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -3,7 +3,14 @@ import { UploadFileModel } from "../models";
 
 class UserController {
   create = (req: express.Request, res: Express.Response) => {
-    const file = {};
+    const file = {
+      filename: String,
+      size: Number,
+      url: String,
+      ext: String,
+      message: { type: Schema.Types.ObjectId, ref: "Message", require: true },
+      user: { type: Schema.Types.ObjectId, ref: "User", require: true },
+    };
     const dialog = new UploadFileModel(postData);
     dialog.save().then((dialogObj: any) => {});
   };
