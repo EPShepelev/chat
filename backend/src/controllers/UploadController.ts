ac1f543b393964efa98a883bdd792a58ddfdd662
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-25 22:38:47 +0300

    modifed upload ctrl

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -5,9 +5,6 @@ class UserController {
   create = (req: express.Request, res: Express.Response) => {
     const file = {};
     const dialog = new UploadFileModel(postData);
-    let image = {};
-    image.url = req.file.url;
-    image.id = req.file.public._id;
   };
   delete = () => {};
 }
