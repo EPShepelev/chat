2011460c8d411c5746e0ed24d8dce607549270df
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-25 22:37:49 +0300

    modifed upload ctrl

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -1,9 +1,9 @@
 import express from "express";
-// import { UploadFileModel } from "../models";
+import { UploadFileModel } from "../models";
 
 class UserController {
   create = (req: express.Request, res: Express.Response) => {
-    console.log(req.file);
+    const dialog = new UploadFileModel(postData);
     let image = {};
     image.url = req.file.url;
     image.id = req.file.public._id;
