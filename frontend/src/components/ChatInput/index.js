fd849bd6447a00738fc6febf87940c044b8f687f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-10-28 23:28:42 +0300

    import upload files

--- frontend/src/components/ChatInput/index.js
+++ frontend/src/components/ChatInput/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import {UploadFiles} from 'components';
 import {
   SmileOutlined,
   CameraOutlined,
