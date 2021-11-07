4278ba30a5a9f03db2ed070de735894b9a3cf39d
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-07 21:56:13 +0300

    refactoring

--- frontend/src/components/ChatInput/index.js
+++ frontend/src/components/ChatInput/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import {UploadFiles} from 'components';
