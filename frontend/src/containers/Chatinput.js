840eaef6665e1a431df29156ddebcfe6d2ae483e
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-09 23:25:25 +0300

    add props

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -57,6 +57,7 @@ const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
       handleSendMessage={handleSendMessage}
       sendMessage={sendMessage}
       attachment={attachment}
+      onSelectFiles={onSelectFiles}
     />
   );
 };
