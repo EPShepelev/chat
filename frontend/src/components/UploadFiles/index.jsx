8b9ab4d442bf929ce6beefd0be6f06a66474cb85
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-13 23:44:52 +0300

    modifed component

--- frontend/src/components/UploadFiles/index.jsx
+++ frontend/src/components/UploadFiles/index.jsx
@@ -37,12 +37,16 @@ const uploadFiles = () => {
       <Upload
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
-        fileList={fileList}
+        fileList={state.fileList}
         onPreview={handlePreview}
         onChange={handleChange}
       ></Upload>
-      <Modal visible={previewVisible} footer={null} onCancel={handleCancel}>
-        <img alt="example" style={{ width: "100%" }} src={previewImage} />
+      <Modal
+        visible={state.previewVisible}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <img alt="example" style={{ width: "100%" }} src={state.previewImage} />
       </Modal>
     </div>
   );
