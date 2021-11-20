0aacc557904500674ffed5380734b5dd2a6b6ebc
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-20 13:37:26 +0300

    modifed component

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -69,7 +69,7 @@ const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
           status: "uploading",
         },
       ];
-      setAttachments();
+      setAttachments(uploaded);
     }
   };
 
