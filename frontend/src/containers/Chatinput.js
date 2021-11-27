fdde2f44012e43b498b50fa32f482b26e2fe5e67
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2021-11-27 14:35:57 +0300

    modifed component

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -40,7 +40,7 @@ const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
     }
   };
 
-  const onUpload = (file) => {
+  const onUpload = (attachments, file) => {
     filesApi.upload(file.file).then(({ data }) => {
       setAttachments(
         attachments.map((item) => {
