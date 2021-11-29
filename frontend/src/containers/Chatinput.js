59e0008748ea7cc4aed8eedbd37e8bff43dfbba7
Шепелев Евгений <shepelev.e@ad1team.ru>
2021-11-29 17:21:21 +0300

    fix

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -41,7 +41,7 @@ const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
   };
 
   const onUpload = (uploaded, file) => {
-    filesApi.upload(file.file).then(({ data }) => {
+    return filesApi.upload(file.file).then(({ data }) => {
       uploaded = [
         ...uploaded,
         {
