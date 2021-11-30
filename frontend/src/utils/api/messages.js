453019f6dc0304ecdcdb3453081fe32c14d621af
Шепелев Евгений <shepelev.e@ad1team.ru>
2021-11-30 17:04:49 +0300

    add atachments to message api

--- frontend/src/utils/api/messages.js
+++ frontend/src/utils/api/messages.js
@@ -3,7 +3,7 @@ import { axios } from "core";
 export default {
   getAllByDialogId: (id) => axios.get("./messages?dialog=" + id),
   removeById: (id) => axios.delete("./messages?id=" + id),
-  send: (text, dialogId) =>
+  send: (text, dialogId, attachments) =>
     axios.post("./messages?dialog=", {
       text: text,
       dialog_id: dialogId,
