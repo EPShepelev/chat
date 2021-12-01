2db5c59a191c88199c9d153eb69e85af4d229340
Шепелев Евгений <shepelev.e@ad1team.ru>
2021-12-01 17:06:26 +0300

    modifed chatinput component

--- frontend/src/containers/Chatinput.js
+++ frontend/src/containers/Chatinput.js
@@ -29,7 +29,7 @@ const ChatInput = ({ fetchSendMessage, currentDialogId }) => {
   };
 
   const sendMessage = () => {
-    fetchSendMessage(value, currentDialogId, attachments);
+    fetchSendMessage(value, currentDialogId, attachments).map(file => (file.uid));
     setValue("");
   };
 
