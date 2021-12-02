6f008833b1bdef5795a9578abe9831fa88698e02
Шепелев Евгений <shepelev.e@ad1team.ru>
2021-12-02 17:00:51 +0300

    fix

--- frontend/src/redux/actions/messages.js
+++ frontend/src/redux/actions/messages.js
@@ -36,7 +36,7 @@ const Actions = {
   },
   fetchSendMessage: (text, dialogId, attachments) => dispatch => {
     messagesApi.send(text, dialogId, attachments);
-  }
+  },
   setIsLoading: (bool) => ({
     type: "MESSAGES:SET_IS_LOADING",
     payload: bool,
